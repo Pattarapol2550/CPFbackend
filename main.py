8f010a05c61924148bf1364a11df3c1774b0dc43
Pattarapol Jamsai <6833212121@student.chula.ac.th>
2026-05-22 21:05:04 +0700

    allow all link for dev:

--- main.py
+++ main.py
@@ -15,14 +15,12 @@ app = FastAPI(title="Ammonia Compressor 7-Set Smart Diagnostics API (Enforced TH
 # เปิดสิทธิ์ CORS ให้หน้าบ้านเชื่อมต่อข้ามโดเมนได้ราบรื่น
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://cpf-frontend-thermo.vercel.app",
-                   "http://127.0.0.1:5500/Frontend/index.html",
-                   "http://127.0.0.1:5500"], # ระบุ URL หน้าบ้านให้ตรงเป๊ะ
+    allow_origins=["*"], # ระบุ URL หน้าบ้านให้ตรงเป๊ะ
     allow_credentials=True,
     allow_methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"], # ระบุ Method ให้ครบ
     allow_headers=["*"], # ยอมรับทุก Header
 )
-
+#https://cpf-frontend-thermo.vercel.app
 # การเชื่อมต่อฐานข้อมูล MongoDB 
 load_dotenv()
 
